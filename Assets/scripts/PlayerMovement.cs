fe49f0d5f922c29545d8718e4fcc1227c7cfebb9
Hycarius <lucazach04@googlemail.com>
2023-02-03 21:16:25 +0100

    Falling cube + scene

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NiceObjectScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+
+    public int speed = 13;
+
     // Start is called before the first frame update
-
-    
-
     void Start()
     {
         
