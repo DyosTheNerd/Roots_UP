682cc24b01aa1707dcb3df2b88813739fadcaaca
DyosTheNerd <irondwarf@gmx.de>
2023-02-03 21:31:23 +0100

    move camera into player and add ui o2 bar

--- Assets/scripts/O2Bar.cs
+++ Assets/scripts/O2Bar.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NiceObjectScript : MonoBehaviour
+public class O2Bar : MonoBehaviour
 {
     // Start is called before the first frame update
-
-
-
+    public GameObject lifebarVisualizer;
+    
     void Start()
     {
         
