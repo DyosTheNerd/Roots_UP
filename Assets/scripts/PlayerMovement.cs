13dcde0d0e1d2faced60c9836c9c4303b6eeaca5
Hycarius <lucazach04@googlemail.com>
2023-02-04 00:55:39 +0100

    No more walljumps, wallhugging and sliding for now

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public int speed = 16;
-    public float jumppower = 5;
-    public int gravityscale = 1;
+    public int speed = 30;
+    public float jumppower = 12;
+    public int gravityscale = 2;
     public float jumptimer = .5f;
 
     bool isGrounded = false;
     Rigidbody2D rb;
-    CapsuleCollider2D mainCollider;
-    
+    BoxCollider2D mainCollider;
+    CapsuleCollider2D capsuleCollider;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
         rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-        mainCollider = GetComponent<CapsuleCollider2D>();
+
+        mainCollider = GetComponent<BoxCollider2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
         rb.gravityScale = gravityscale;
     }
 
@@ -58,18 +62,21 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(movedirection * speed,rb.velocity.y);
+        rb.AddForce(new Vector2(movedirection * speed,rb.velocity.y));
+        //rb.velocity = new Vector2(movedirection * speed,rb.velocity.y);
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -30, 30), rb.velocity.y);
 
-        Bounds colliderbounds = mainCollider.bounds;
-        float colliderradius = mainCollider.size.x;
-        Vector3 groundCheckPos = colliderbounds.min + new Vector3(mainCollider.size.x/2, mainCollider.size.y/2, 0);
+
+        Vector3 groundCheckPos = mainCollider.transform.position + new Vector3(mainCollider.offset.x, mainCollider.offset.y, 0);
         // Check if player is groundeded
         //Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckPos, colliderradius);
-        Collider2D colliders = Physics2D.OverlapCircle(groundCheckPos, colliderradius, LayerMask.GetMask("Default"));
+        //Collider2D colliders = Physics2D.OverlapBox(mainCollider.transform.position, mainCollider.size, 0,  LayerMask.GetMask("Default"));
+        
         // Check if any of the overlapping colliders are not player collider, if so set isGrounded to true
         isGrounded = false;
-        if (colliders.IsTouchingLayers())
+        if (mainCollider.IsTouchingLayers(LayerMask.GetMask("Default")))
         {
+            //Debug.DrawLine(groundCheckPos, new Vector3(), Color.blue);
             isGrounded = true;
         }
 
@@ -85,8 +92,9 @@ public class PlayerMovement : MonoBehaviour
             }
         }*/
 
+        Vector3 bottomleft = groundCheckPos - new Vector3(mainCollider.size.x, mainCollider.size.y, 0)/2;
         // Simple debug
-        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, colliderradius, 0), isGrounded ? Color.green : Color.red);
-        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(colliderradius, 0, 0), isGrounded ? Color.green : Color.red);
+        Debug.DrawLine(bottomleft, bottomleft + new Vector3(0, mainCollider.size.y, 0), isGrounded ? Color.green : Color.red);
+        Debug.DrawLine(bottomleft, bottomleft + new Vector3(mainCollider.size.x, 0, 0), isGrounded ? Color.green : Color.red);
     }
 }
