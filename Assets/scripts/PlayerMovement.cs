4d9c9cf5559e77b78bbbda68b08210dce671ff02
LAPTOP-B8JIDT8U\alexa <alexander_rosenthal@web.de>
2023-02-05 15:11:43 +0100

    First Step Jump FIx

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -102,7 +102,6 @@ public class PlayerMovement : MonoBehaviour
             if (!Input.GetKey(KeyCode.W))
             {
                 sincejump = 0;
-                animator.SetBool("jumped", true);
             }
             
             if (Input.GetKey(KeyCode.S))
@@ -115,6 +114,16 @@ public class PlayerMovement : MonoBehaviour
             {
                 jumpdebounce = false;
                 timeelapsed = 0;
+            }
+
+
+            if (isGrounded == false)
+            {
+                animator.SetBool("jumped", true);
+            }
+            else
+                {
+                animator.SetBool("jumped", false);
             }
 
             animator.SetInteger("run", movedirection);
@@ -167,7 +176,6 @@ public class PlayerMovement : MonoBehaviour
             isGrounded = true;
             jumped = false;
 
-            animator.SetBool("jumped", false);
         }
         canClimb = false;
         if (mainCollider.IsTouchingLayers(LayerMask.GetMask("Climbable")))
